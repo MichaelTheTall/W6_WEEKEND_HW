026cd34571c9954a7b33cf6d0238e22943e65113
MichaelTheTall <michaeljhnewman@gmail.com>
2018-09-07 11:52:48 +0100

    app form inputs

--- js/app.js
+++ js/app.js
@@ -19,3 +19,28 @@ const handleNewItemFormSubmit = function(event) {
 
   event.target.reset();
 }
+
+const createDoggoListItem = function(form) {
+  const doggoListItem =
+  document.createElement('li');
+  doggoListItem.classList.add('doggolist-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  doggoListItem.appendChild(name);
+
+  const gender = document.createElement('h3');
+  gender.textContent = form.gender.value;
+  doggoListItem.appendChild(gender);
+
+  const breed = document.createElement('p');
+  breed.textContent = form.breed.value;
+  doggoListItem.appendChild(breed);
+
+  return doggoListItem;
+}
+
+const handleDeleteAllClick = function(event) {
+  const doggoList = document.querySelector('#doggolist');
+  doggoList.innerHTML = '';
+}
